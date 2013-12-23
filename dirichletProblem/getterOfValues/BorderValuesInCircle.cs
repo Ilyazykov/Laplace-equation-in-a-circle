755b957fd39197c5b3cbdc0875a25f12f7f00b0e
Ilyazykov <ilya.zykov52@gmail.com>
2013-12-23 19:47:35 +0400

    Получены гу в круге

--- dirichletProblem/getterOfValues/BorderValuesInCircle.cs
+++ dirichletProblem/getterOfValues/BorderValuesInCircle.cs
@@ -26,16 +26,22 @@ namespace dirichletProblem.getterOfValues
             double stepX = (rectangle.endX - rectangle.beginX) / (numPointX - 1);
             double stepY = (rectangle.endY - rectangle.beginY) / (numPointY - 1);
 
+            double radius = (rectangle.endX - rectangle.beginX) / 2;
+
             for (int x = 0; x < numPointX; x++)
             {
-                bottom.Add(u.getValue(rectangle.beginX + x * stepX, rectangle.beginY)); //TODO изменить чтобы попадать в круг
-                top.Add(u.getValue(rectangle.beginX + x * stepX, rectangle.endY)); //TODO изменить чтобы попадать в круг
+                double realX = rectangle.beginX + x * stepX;
+
+                bottom.Add(u.getValue(realX, -Math.Sqrt(radius*radius - realX*realX)));
+                top.Add(u.getValue(realX, Math.Sqrt(radius * radius - realX * realX)));
             }
 
             for (int y = 0; y < numPointY; y++)
             {
-                left.Add(u.getValue(rectangle.beginX, rectangle.beginY + y * stepY)); //TODO изменить чтобы попадать в круг
-                right.Add(u.getValue(rectangle.endX, rectangle.beginY + y * stepY)); //TODO изменить чтобы попадать в круг
+                double realY = rectangle.beginY + y * stepY;
+
+                left.Add(u.getValue(-Math.Sqrt(radius * radius - realY * realY), realY));
+                right.Add(u.getValue(Math.Sqrt(radius * radius - realY * realY), realY));
             }
         }
     }
